332e279204c6c33622abb7f9c89f8e1664f43b06
Xavier <sseingalt@gmail.com>
2015-12-19 19:59:00 +0100

    create a Render trait object

--- src/gui/window.rs
+++ src/gui/window.rs
@@ -11,18 +11,24 @@ pub struct Window {
     size_x: i32,
     size_y: i32,
 
-    name: String,
     handle: WINDOW, // ncurses subwin handle
+
+    renderer: Box<Render>,
 }
 
 impl Window {
-    pub fn new(name: String, start_y: i32, start_x: i32, size_y: i32, size_x: i32) -> Window {
+    pub fn new(start_y: i32,
+               start_x: i32,
+               size_y: i32,
+               size_x: i32,
+               renderer: Box<Render>)
+               -> Window {
         Window {
-            name: name,
             start_x: start_x,
             start_y: start_y,
             size_x: size_x,
             size_y: size_y,
+            renderer: renderer,
             handle: stdscr, // hack init the subwin to the whole window
         }
     }
@@ -41,4 +47,31 @@ impl Window {
     pub fn write(&self, text: String) {}
 
     pub fn refresh(&self) {}
+}
+
+pub trait Render {
+    fn render();
+}
+
+struct StoryWin;
+struct MapWin;
+struct StatusWin;
+
+impl Render for StoryWin {
+    fn render() {
+        println!("I am story!");
+    }
+}
+
+
+impl Render for MapWin {
+    fn render() {
+        println!("I'm map!");
+    }
+}
+
+impl Render for StatusWin {
+    fn render() {
+        println!("I'm program");
+    }
 }
