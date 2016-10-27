031a3bb47528914c882932af9c6a17b706acf7e0
Xavier <sseingalt@gmail.com>
2016-10-27 21:53:43 +0200

    add structure for processing in multiple input modes

--- src/ui/input.rs
+++ src/ui/input.rs
@@ -1,10 +1,12 @@
+#![allow(unused_variables)]
 
 /// Mode of input similar to Vim input mode.
 ///
 /// In each mode the key will have a different meaning
+#[derive(Debug)]
 pub enum InputMode {
-    /// Default game mode
-    Default,
+    /// Normal game mode
+    Normal,
     /// Move cursor freely around to identify things
     Explore,
     /// Menu
@@ -21,7 +23,7 @@ pub struct InputHandler {
 
 impl InputHandler {
     pub fn new() -> InputHandler {
-        InputHandler { mode: InputMode::Default }
+        InputHandler { mode: InputMode::Normal }
     }
 
     pub fn change_mode(&mut self, new_mode: InputMode) {
@@ -29,7 +31,16 @@ impl InputHandler {
     }
 
     pub fn process_key(&self, key: char) {
+        match self.mode {
+            InputMode::Normal => self.process_normal(key),
+            InputMode::Explore => self.process_explore(key),
+            InputMode::Menu => self.process_menu(key),
+            InputMode::YesOrNo => self.process_yesorno(key),
+            InputMode::Help => self.process_help(key),
+        }
+    }
 
+    fn process_normal(&self, key: char) {
         match key {
             'h' => info!("west"),
             'j' => info!("south"),
@@ -39,8 +50,23 @@ impl InputHandler {
             'u' => info!("left"),
             'b' => info!("left"),
             'n' => info!("left"),
-            _ => info!("error"),
+            _ => warn!("unknown command in {:?} mode : {}", self.mode, key),
         }
+    }
 
+    fn process_explore(&self, key: char) {
+        unimplemented!();
+    }
+
+    fn process_menu(&self, key: char) {
+        unimplemented!();
+    }
+
+    fn process_yesorno(&self, key: char) {
+        unimplemented!();
+    }
+
+    fn process_help(&self, key: char) {
+        unimplemented!();
     }
 }
